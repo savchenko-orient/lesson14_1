a023b97d492df677584878a351f6273f2d46a9cd
Oleksii Savchenko <savchenko.orient@gmail.com>
2021-01-05 14:02:43 +0200

    Создал классы First и Second, где первый наследует от второго метод hello().

--- script.js
+++ script.js
@@ -1,37 +1,20 @@
 'use strict';
-function DomElement(selector = '#someClass', height = '200px', width = '175px', bg = 'red', fontSize = '30px') {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
+class First {
+    hello() {
+        console.log('Я метод родителя!');
+    }
 
-    this.createElement = function () {
-        let i = this.selector;
-        if (i.startsWith('.') === true) {
-            let newElem = document.createElement('div');
-            newElem.className = i;
-            newElem.style.cssText = `height: ${this.height}; width:${this.width}; background-color: ${this.bg}; font-size: ${this.fontSize}`;
-            newElem.innerText = prompt('What is my name?');
-            document.body.append(newElem);
-            
-        } else if (i.startsWith('#') === true) {
-            let newElem = document.createElement('p');
-            newElem.id = i;
-            newElem.style.cssText = `height: ${this.height}; width:${this.width}; background-color: ${this.bg}; font-size: ${this.fontSize}`;
-            newElem.innerText = prompt('What is my name?');
-            document.body.append(newElem);
-            
-        }
-        
-
-    };
 }
 
-let elem = new DomElement('.classDiv', '100px', '300px', 'green', '25px');
-let elem2 = new DomElement('#idP', '50px', '500px', 'blue', '30px');
-elem.createElement();
-elem2.createElement();
+class Second extends First {
+    hello() {
+        super.hello();
+        console.log('А я наследуемый метод!');
+    }
+}
+
+const first = new First();
+const second = new Second();
 
 
-   
+second.hello();
